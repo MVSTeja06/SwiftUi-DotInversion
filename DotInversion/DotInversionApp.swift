9fdba847a1e17b26b4a0db973f70bf706967693b
amatcha <amatc001@odu.edu>
2021-10-28 19:29:26 -0400

    Initial Commit

---
+++ DotInversion/DotInversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  DotInversionApp.swift
+//  DotInversion
+//
+//  Created by Ajay Gupta on 10/28/21.
+//
+
+import SwiftUI
+
+@main
+struct DotInversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
